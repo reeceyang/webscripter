49ea5cf02d51844f4321a868fa7ba9342bc2be77
reeceyang <reeceyang@live.com>
2021-01-03 23:07:02 -0600

    use asyncfunction constructor

---
+++ pi.js
@@ -0,0 +1,11 @@
+var loops = await input("number of loops to run? ");
+var pi = 0;
+for (var i = 1; i < loops; i++) {
+  if (i % 2 == 0) {
+    pi += 4 * (1.0 / (2 * i - 1));
+  } else {
+    pi -= 4 * (1.0 / (2 * i - 1));
+  }
+  println(i, pi);
+  //await input();
+}
